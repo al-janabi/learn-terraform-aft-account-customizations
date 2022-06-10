ab4e38adbeca1b9a7b758f15cf56c320f77a1450
Ali Al-Janabi <ajanabi@amazon.com>
2022-06-10 13:46:55 -0500

    adding the ID

--- workload/terraform/data.tf
+++ workload/terraform/data.tf
@@ -2,6 +2,8 @@ data "aws_caller_identity" "current" {}
 
 data "aws_availability_zones" "available" {}
 
-data "aws_ec2_transit_gateway" "tgw" {}
+data "aws_ec2_transit_gateway" "tgw" {
+    id = "tgw-01072e3d1507f755f"
+}
 
 data "aws_vpc_ipam_pool" "ipam_pool" {}
